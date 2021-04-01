51172ef9e41b541cc1b4e8147914ed61173a93d1
Steven <stevenhuyn@gmail.com>
2021-04-01 14:59:51 +1100

    Add arrow shooting capability to pebble

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,24 +1,14 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.block.Blocks;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ExampleMod implements ModInitializer {
 
-	public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier("tutorial", "general"),
-			() -> new ItemStack(Blocks.COBBLESTONE));
-
-	public static final ItemGroup OTHER_GROUP = FabricItemGroupBuilder.create(new Identifier("tutorial", "other"))
-			.icon(() -> new ItemStack(Items.BOWL)).build();
-
-	public static final FabricPebble FABRIC_PEBBLE = new FabricPebble(new FabricItemSettings().group(OTHER_GROUP));
+	public static final FabricPebble FABRIC_PEBBLE = new FabricPebble(new FabricItemSettings().group(ItemGroup.COMBAT));
 
 	@Override
 	public void onInitialize() {
--- src/main/java/net/fabricmc/example/FabricPebble.java
+++ src/main/java/net/fabricmc/example/FabricPebble.java
@@ -1,14 +1,22 @@
 package net.fabricmc.example;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.item.ArrowItem;
+import net.minecraft.item.BowItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.LiteralText;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
-public class FabricPebble extends Item {
+public class FabricPebble extends BowItem {
+
+    public static float baseForce = 10f;
+    public static float chargeTime = 10f;
 
     public FabricPebble(Settings settings) {
         super(settings);
@@ -16,7 +24,22 @@ public class FabricPebble extends Item {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
-        playerEntity.playSound(SoundEvents.BLOCK_WOOL_BREAK, 1.0F, 1.0F);
-        return TypedActionResult.success(playerEntity.getStackInHand(hand));
+        ItemStack itemStack = playerEntity.getStackInHand(hand);
+        playerEntity.setCurrentHand(hand);
+        return TypedActionResult.consume(itemStack);
+    }
+
+    @Override
+    public void onStoppedUsing(ItemStack itemStack, World world, LivingEntity livingEntity, int remainingUseTicks) {
+        ArrowItem arrowItem = (ArrowItem) ((ArrowItem) (itemStack.getItem() instanceof ArrowItem ? itemStack.getItem()
+                : Items.ARROW));
+
+        PersistentProjectileEntity persistentProjectileEntity = arrowItem.createArrow(world, itemStack, livingEntity);
+
+        persistentProjectileEntity.setCustomName(new LiteralText("Pebble"));
+
+        persistentProjectileEntity.setProperties(livingEntity, livingEntity.pitch, livingEntity.yaw, 0.0f, 1.0f, 1.0f);
+
+        world.spawnEntity(persistentProjectileEntity);
     }
 }
