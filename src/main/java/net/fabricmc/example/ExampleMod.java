9c81c16ca76946766ea902e54e2d4a15f3980604
Steven <stevenhuyn@gmail.com>
2021-04-01 13:41:25 +1100

    Add some test ItemGroups

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,14 +1,24 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ExampleMod implements ModInitializer {
 
-	public static final FabricItem FABRIC_ITEM = new FabricItem(new FabricItemSettings().group(ItemGroup.MISC));
+	public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier("tutorial", "general"),
+			() -> new ItemStack(Blocks.COBBLESTONE));
+
+	public static final ItemGroup OTHER_GROUP = FabricItemGroupBuilder.create(new Identifier("tutorial", "other"))
+			.icon(() -> new ItemStack(Items.BOWL)).build();
+
+	public static final FabricItem FABRIC_ITEM = new FabricItem(new FabricItemSettings().group(OTHER_GROUP));
 
 	@Override
 	public void onInitialize() {
