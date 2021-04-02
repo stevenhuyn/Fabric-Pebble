b307d699e1403d2da48cf956b53dd7a984ab0702
Steven <stevenhuyn@gmail.com>
2021-04-02 18:56:37 +1100

    Add snowball particle effect to flight and impact of pebble

--- src/main/java/net/fabric/pebble/entity/PebbleEntity.java
+++ src/main/java/net/fabric/pebble/entity/PebbleEntity.java
@@ -2,6 +2,8 @@ package net.fabric.pebble.entity;
 
 import net.fabric.pebble.PebbleMod;
 import net.fabric.pebble.client.PebbleModClient;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
@@ -11,7 +13,11 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.BlazeEntity;
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.Packet;
+import net.minecraft.particle.ItemStackParticleEffect;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
@@ -54,6 +60,38 @@ public class PebbleEntity extends ThrownItemEntity {
             ((LivingEntity) entity).addStatusEffect((new StatusEffectInstance(StatusEffects.SLOWNESS, 20 * 3, 2)));
             ((LivingEntity) entity).addStatusEffect((new StatusEffectInstance(StatusEffects.POISON, 20 * 3, 1)));
             entity.playSound(SoundEvents.BLOCK_SNOW_STEP, 2F, 1F);
+        }
+    }
+
+    @Environment(EnvType.CLIENT)
+    // Not entirely sure, but probably has do to with the snowball's particles.
+    // (OPTIONAL)
+    private ParticleEffect getParticleParameters() {
+        ItemStack itemStack = this.getItem();
+        return (ParticleEffect) (itemStack.isEmpty() ? ParticleTypes.ITEM_SNOWBALL
+                : new ItemStackParticleEffect(ParticleTypes.ITEM, itemStack));
+    }
+
+    @Environment(EnvType.CLIENT)
+    // Also not entirely sure, but probably also has to do with the particles.
+    // This method (as well as the previous one) are optional, so if you don't
+    // understand, don't include this one.
+    public void handleStatus(byte status) {
+        if (status == 3) {
+            ParticleEffect particleEffect = this.getParticleParameters();
+            for (int i = 0; i < 8; ++i) {
+                this.world.addParticle(particleEffect, this.getX(), this.getY(), this.getZ(), 0.0D, 0.0D, 0.0D);
+            }
+        }
+
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        ParticleEffect particleEffect = this.getParticleParameters();
+        for (int i = 0; i < 8; ++i) {
+            this.world.addParticle(particleEffect, this.getX(), this.getY(), this.getZ(), 0.0D, 0.0D, 0.0D);
         }
     }
 
