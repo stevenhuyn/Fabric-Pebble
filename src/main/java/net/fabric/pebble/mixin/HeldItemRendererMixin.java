d0b4663435d35246f4704b69b8ebb34ae1d95a79
Steven <stevenhuyn@gmail.com>
2021-04-12 00:55:25 +1000

    Add mixin injection test

--- src/main/java/net/fabric/pebble/mixin/HeldItemRendererMixin.java
+++ src/main/java/net/fabric/pebble/mixin/HeldItemRendererMixin.java
@@ -1,6 +1,13 @@
 package net.fabric.pebble.mixin;
 
+import net.fabric.pebble.PebbleMod;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.item.HeldItemRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Arm;
+import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -8,8 +15,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HeldItemRenderer.class)
 public class HeldItemRendererMixin {
-	@Inject(at = @At("HEAD"), method = "renderFirstPersonItem")
-	private void init(CallbackInfo info) {
-		System.out.println("Bro...");
+	@Inject(at = @At(value="INVOKE", target="Lnet/minecraft/client/util/math/MatrixStack;push()V"), method = "renderFirstPersonItem", cancellable = true)
+	private void init(AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo info) {
+		boolean bl = hand == Hand.MAIN_HAND;
+		Arm arm = bl ? player.getMainArm() : player.getMainArm().getOpposite();
+		if (item.getItem() == PebbleMod.PebbleItem) {
+			System.out.println("You are holding a pebble!");
+
+			matrices.pop();
+
+			// Injects a return essentially
+			info.cancel();
+		}
 	}
 }
